9fb20d898091f789da551f02d75f154264bb3510
Dag Lovenvald <clmbrdude@gmail.com>
2017-09-28 13:48:27 +0200

    remove screen-layout

--- i3/screen-layout.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DisplayPort-0 --off --output DisplayPort-1-3 --mode 1920x1200 --pos 0x0 --rotate normal --output DVI-1-1 --off --output DisplayPort-0-1 --mode 1920x1200 --pos 3840x0 --rotate normal --output DisplayPort-0-8 --primary --mode 1920x1200 --pos 1920x0 --rotate normal --output DVI-0 --off
